c1b09cd0a37a2fd1b49b43a18d29cf9ca5a6f2b9
André Ruffert <a.ruffert@gmail.com>
2013-05-11 23:27:42 +0200

    added gitshots.rb

---
+++ gitshots.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# Requires imagesnap from https://github.com/alexwilliamsca/imagesnap (brew install imagesnap)
+# Create a .gitshots directory at the same level as the .git directory.
+# To assemble the video use http://www.dayofthenewdan.com/projects/tlassemble
+
+path=File.dirname(__FILE__)
+file="#{path}/../../.gitshots/#{Time.now.to_i}.jpg"
+puts "Taking capture into #{file}!"
+system "imagesnap -q -w 3 #{file}"
+exit 0
